81fc9998526c5cc4d9d9e6ba6b4a12a02210a93c
ysk <ysk@sk.com>
2016-02-05 21:28:13 +0900

    action tab pager and sliding drawer

---
+++ AndroidExample/Exam05_ViewPager/src/com/example/exam05_viewpager/TabInfo.java
@@ -0,0 +1,16 @@
+package com.example.exam05_viewpager;
+
+import android.os.Bundle;
+
+public class TabInfo {
+	
+	public Class<?> 	cls = null;
+	public Bundle	bun = null;
+	
+	public TabInfo(Class<?> cls , Bundle bun)
+	{
+		this.cls = cls;
+		this.bun = bun;
+	}
+
+}
