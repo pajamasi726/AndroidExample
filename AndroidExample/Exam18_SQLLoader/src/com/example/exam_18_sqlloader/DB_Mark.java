41a4f5c7d376d0b5b60ca2439c02d99b2fd970c7
ysk <ysk@sk.com>
2016-02-25 14:51:25 +0900

    provider add

---
+++ AndroidExample/Exam18_SQLLoader/src/com/example/exam_18_sqlloader/DB_Mark.java
@@ -0,0 +1,9 @@
+package com.example.exam_18_sqlloader;
+
+public final class DB_Mark {
+	public static final String DB_NAME = "pserson1.db";
+	public static final String DB_COL_NAME = "name";
+	public static final String DB_TABLE = "Person1";
+	public static final String SQL_CREATE = "create table "+DB_TABLE+" (_id INTEGER PRIMARY KEY AUTOINCREMENT, "+DB_COL_NAME+" TEXT);";
+	
+}
